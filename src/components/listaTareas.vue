0b0092109e7a5f1551fa9cce7b49325567c462c3
Vanessa_Riquelme <vriquelmefe@gmail.com>
2021-07-21 18:57:34 -0400

    subiendo proyecto

---
+++ src/components/listaTareas.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="lista">
+      <h2> Crea una nueva tarea</h2>
+      <label for="tarea" ><b>Tarea</b></label>
+      <input :type="text" v-model="nuevasTareas" placeholder="Ingresa nueva tarea">
+      <button @click="agregarTarea">Crear</button>
+    <h4>Lista</h4>
+    <ul >
+      <li v-for="(value, $index) in tareas" :key="$index">{{ $index }}: {{ value }}</li> 
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+      nuevasTareas: '',
+      tareas: [],
+  }),
+     methods: {
+      agregarTarea() {
+        this.tareas.push( this.nuevasTareas )
+        this.nuevasTareas = ''
+      }
+  }
+}
+
+</script>
+<style scoped>
+  ul{
+    margin: 0 auto;
+    text-align: left;
+    padding-left: 15% ;
+    width: 20%;
+  }
+</style>
